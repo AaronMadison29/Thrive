9181e411bb372e3cb10d0dcc2a1cd445f318bf9e
Aaron Madison <aaronmadison29@gmail.com>
2020-03-05 15:27:53 -0600

    User Roles migrated to database

--- ThriveAPP/Data/ApplicationDbContext.cs
+++ ThriveAPP/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,24 @@ namespace ThriveAPP.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>()
+            .HasData(
+            new IdentityRole
+            {
+                Name = "Teacher",
+                NormalizedName = "TEACHER"
+            },
+
+            new IdentityRole
+            {
+                Name = "Student",
+                NormalizedName = "STUDENT"
+            }
+            );
         }
     }
 }
---
+++ ThriveAPP/Data/Migrations/20200305212345_UserRolls.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThriveAPP.Data.Migrations
+{
+    public partial class UserRolls : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1b7156cb-dce8-4aae-a962-b4f0a124182d", "34cb8508-cc36-4ba0-b194-103e8b0234e1", "Teacher", "TEACHER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "de5711bd-2e5d-4587-9ce2-b855840d7e7e", "2c55346a-95d6-440d-8f48-fa4c3e2de83c", "Student", "STUDENT" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1b7156cb-dce8-4aae-a962-b4f0a124182d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "de5711bd-2e5d-4587-9ce2-b855840d7e7e");
+        }
+    }
+}
