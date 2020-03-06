620f812dc4a72475482890e1171f8484a8745344
unknown <svclennan@icloud.com>
2020-03-06 08:34:25 -0600

    made some of the foreign keys on the models nullable

--- Repository/Models/Student.cs
+++ Repository/Models/Student.cs
@@ -15,13 +15,14 @@ namespace SchoolAPI.Models
         public string Name { get; set; }
         public List<Class> Classes { get; set; }
         public string ParentEmail { get; set; }
+        public string Email { get; set; }
 
         [ForeignKey("Profile")]
-        public int ProfileId { get; set; }
+        public int? ProfileId { get; set; }
         public Profile Profile { get; set; }
 
         [ForeignKey("Parent")]
-        public int ParentId { get; set; }
+        public int? ParentId { get; set; }
         public Parent Parent { get; set; }
     }
 }
