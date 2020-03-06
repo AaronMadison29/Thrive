eaa5e0cc74745f6d53044aab6bd16e7c0a6f3a9f
unknown <svclennan@icloud.com>
2020-03-06 08:06:00 -0600

    moved the models to the repository

---
+++ Repository/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolAPI.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public List<Class> Classes { get; set; }
+        public string ParentEmail { get; set; }
+
+        [ForeignKey("Profile")]
+        public int ProfileId { get; set; }
+        public Profile Profile { get; set; }
+
+        [ForeignKey("Parent")]
+        public int ParentId { get; set; }
+        public Parent Parent { get; set; }
+    }
+}
---
+++ Repository/Models/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolAPI.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int TeacherId { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Subject { get; set; }
+
+        [NotMapped]
+        public List<Class>  Classes { get; set; }
+    }
+}
