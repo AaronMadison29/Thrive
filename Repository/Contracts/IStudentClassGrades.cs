1fd79265fd810066e3a05d0e639fc2596ac03f6c
Aaron Madison <aaronmadison29@gmail.com>
2020-03-06 11:44:11 -0600

    Added repo for StudentClassGrades

---
+++ Repository/Contracts/IStudentClassGrades.cs
@@ -0,0 +1,11 @@
+﻿using SchoolAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Contracts
+{
+    interface IStudentClassGrades : IRepositoryBase<StudentClassGrades>
+    {
+    }
+}
