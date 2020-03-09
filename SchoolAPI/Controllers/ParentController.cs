a38f26735fc2d134934926558b4107f4be91d71d
unknown <svclennan@icloud.com>
2020-03-09 09:30:33 -0500

    changed the routing of the parent and student get methods when searching by userId

--- SchoolAPI/Controllers/ParentController.cs
+++ SchoolAPI/Controllers/ParentController.cs
@@ -34,7 +34,7 @@ namespace SchoolAPI.Controllers
         }
 
         // GET: api/Teacher/5
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public Parent Get(int id)
         {
             return _repo.Parents.GetParentIncludeAll(id);
